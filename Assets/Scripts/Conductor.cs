ea93fb3c41205a37124aeb061b802243685701c1
Alden Smith <Aldensmith06@Gmail.com>
2026-07-15 23:41:44 -0500

    Conductor for managing scenes

---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,46 @@
+using System.IO;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    // singleton instance
+    public static Conductor Instance { get; private set; }
+
+
+
+    [SerializeField] private AudioSource songAudioSource; // The AudioSource component that will play the music
+    [SerializeField] private float audioOffset = 0.00f; // Sometimes a songs beat doesnt start the instant the song starts
+
+    public float bpm = 120f; // 120 is common idk I need something to start with
+    public float currentSongTime { get; private set; } // The current time of the song in seconds
+    public float currentBeat { get; private set; }
+
+    private float secPerBeat; // The number of seconds per beat, calculated from the BPM
+
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        secPerBeat = 60f / bpm;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        songAudioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!songAudioSource.isPlaying) return; // Wait for the song to start before updating
+
+        float rawAudioTime = (float)songAudioSource.timeSamples / songAudioSource.clip.frequency; // Get the current time of the song in seconds
+
+        currentSongTime = rawAudioTime - audioOffset;
+
+        currentBeat = currentSongTime / secPerBeat;
+    }
+}

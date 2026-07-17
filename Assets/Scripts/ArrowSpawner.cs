30a60c37b0b80e61fe8faa47a175d422fba456f8
Alden Smith <Aldensmith06@Gmail.com>
2026-07-17 10:23:44 -0500

    Arrow spawner with lanes

--- Assets/Scripts/ArrowSpawner.cs
+++ Assets/Scripts/ArrowSpawner.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ArrowSpawner : MonoBehaviour
 {
-    [System.Serializable]
-    public struct NoteData
-    {
-        public float targetBeat; // What beat to step on this note
-        public int laneIndex;    // 0 = Left, 1 = Down, 2 = Up, 3 = Right
-    }
+    //[System.Serializable]
+    //public struct NoteData
+    //{
+    //    public float targetBeat; // What beat to step on this note
+    //    public int laneIndex;    // 0 = Left, 1 = Down, 2 = Up, 3 = Right
+    //}
 
     [Header("Prefabs & Spawn Positions")]
     [SerializeField] private GameObject[] arrowPrefabs; // Array of 4 arrow prefabs
@@ -18,16 +19,27 @@ public class ArrowSpawner : MonoBehaviour
     [SerializeField] private float beatsAheadToSpawn = 4f; // Spawn arrows 4 beats before they must be hit
 
     // A list of notes representing your level chart (ordered by targetBeat)
-    private List<NoteData> songChart = new List<NoteData>();
+    private List<GameObject> songChart = new List<GameObject>();
     private int nextNoteIndex = 0;
 
     private void Start()
     {
         // For testing: Create a simple chart (Spawn notes on beats 1, 2, 3, 4)
-        songChart.Add(new NoteData { targetBeat = 1f, laneIndex = 0 });
-        songChart.Add(new NoteData { targetBeat = 2f, laneIndex = 1 });
-        songChart.Add(new NoteData { targetBeat = 3f, laneIndex = 2 });
-        songChart.Add(new NoteData { targetBeat = 4f, laneIndex = 3 });
+        GameObject arrow = Instantiate(arrowPrefabs[0], spawnPoints[0].transform.position, Quaternion.identity);
+        arrow.GetComponent<ArrowBehaviour>().TargetBeat = 1f;
+        songChart.Add(arrow);
+
+        GameObject arrow2 = Instantiate(arrowPrefabs[0], spawnPoints[1].transform.position, Quaternion.identity);
+        arrow2.GetComponent<ArrowBehaviour>().TargetBeat = 2f; 
+        songChart.Add(arrow2);
+
+        GameObject arrow3 = Instantiate(arrowPrefabs[0], spawnPoints[2].transform.position, Quaternion.identity);
+        arrow3.GetComponent <ArrowBehaviour>().TargetBeat = 3f;
+        songChart.Add(arrow3);
+
+        GameObject arrow4 = Instantiate(arrowPrefabs[0], spawnPoints[3].transform.position, Quaternion.identity);
+        arrow4.GetComponent<ArrowBehaviour>().TargetBeat = 4f;
+        songChart.Add(arrow4);
     }
 
     private void Update()
